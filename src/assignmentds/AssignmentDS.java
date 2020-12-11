e56dfa4a0f1671fb684258a924feea3f50cf2e4d
Aliff Aiman <aliffaimanridzuan@gmail.com>
2020-12-11 21:14:17 +0800

    init commit

---
+++ src/assignmentds/AssignmentDS.java
@@ -0,0 +1,12 @@
+package assignmentds;
+
+public class AssignmentDS {
+
+    public static void main(String[] args) {
+        
+        System.out.println("This is ds assignment");
+        //comment
+        
+    }
+    
+}
